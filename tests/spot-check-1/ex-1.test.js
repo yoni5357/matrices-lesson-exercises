22649f408c8bacb678a94870c5652de1dd900a58
hunter-aw <hunteraweinsheink@gmail.com>
2019-03-06 15:28:45 +0200

    fixed leftover requirements

--- tests/spot-check-1/ex-1.test.js
+++ tests/spot-check-1/ex-1.test.js
@@ -1,9 +1,3 @@
-import assert from 'assert';
-import App from '../../src/App';
-import Home from '../../src/components/Home';
-import { wrap } from 'module';
-
-
 describe("exercise1", () => {
     it('should pass', () => {
         const number = 4
