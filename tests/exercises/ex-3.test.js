01a4b26d4eddde30da4a71aecabd76ada1f3eaca
danbrud <dannybrudner@users.noreply.github.com>
2019-09-23 18:07:45 +0300

    added data.json file for salaryData

--- tests/exercises/ex-3.test.js
+++ tests/exercises/ex-3.test.js
@@ -1,4 +1,5 @@
 let EmployeeMatrix
+let salaryData = require('../../data.json')
 
 try {
     EmployeeMatrix = require('../../src/EmployeeMatrix')
@@ -8,15 +9,6 @@ try {
 
 describe('exercise3', () => {
     it(`should create a method 'findCoordinate' in your matrix which receives a value and returns the coordinate of that value' `, function () {
-        let salaryData = [
-            { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
-            { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
-            { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
-            { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
-            { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
-            { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
-        ]
-
         const em = new EmployeeMatrix()
         em.loadData(salaryData)
 
